8aaba59b6ff2055985d11162851299ffec945ff5
leekycauldron <brysonleekwen@.gmailcom>
2022-01-01 18:02:51 -0500

    Some code cleanup along with the ability to tell the difference between a cola and fries

--- classifier.py
+++ classifier.py
@@ -39,68 +39,32 @@ def getOrderItems(img):
     final = cv2.bitwise_xor(gray,im_floodfill)
     
     blur = cv2.GaussianBlur(final,(9,9), 0)
-    imgCanny = cv2.Canny(blur, 300, 300//3)
+    imgCanny = cv2.Canny(blur, 100, 100//3)
     
 
     # Get number of contours (order items)
     contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     itm_cnt = 0
     item_coords = []
+    item_corners = []
     for cnt in contours:
-        area = cv2.contourArea(cnt)
-        #print("AREA: " + str(area))
+   
         x, y, w, h = cv2.boundingRect(cnt)
-        #print("ACTUAL:" + str((x+w)*(y+h)))
-        #if area >= 13.5 and (x+w)*(y+h) >= 8000: # Parts of food accidentally detected sometimes.
-        if True:
+
+        # Get corners in item (used to classify order item)
+        peri = cv2.arcLength(cnt,True)
+        approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+        objCorners = len(approx)
+        item_corners.append(objCorners)
+
+
+        itm_cnt += 1
+        item_coords.append([(x,x+w),(y,y+h)])
+
+        """cv2.drawContours(img, cnt, -1, (0, 0, 0), 3)
             
-           
-            if len(item_coords) != 0:
-                for coord in range(len(item_coords)):
-                    if abs(item_coords[coord][0][0] - x) < 50: # Check if the box is inside another box
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)"""
                         
-                        print("{} is less than {}".format(x,item_coords[coord][0][0]))
-                        # Sometimes false positive contours are added before real contours
-                        # To prevent this, checking the shape area will allow me to find the right contour.
-                        if (x+w)*(y+h) > item_coords[coord][0][1] * item_coords[coord][1][1]: 
-                            
-                            print("Removing detected false positive... {} is bigger than {}".format((x+w)*(y+h),item_coords[coord][0][1] * item_coords[coord][1][1]))
-                            
-            
-                            cv2.rectangle(img, (item_coords[coord][0][0], item_coords[coord][0][1]),(item_coords[coord][1][0], item_coords[coord][1][1]), (0,0,255), 2)
-
-                            item_coords.pop(coord) # The previously added contour was the imposter (sus).
-                            # Dont add another count to itm_cnt since net gain is 0 (-1 then +1).
-                            item_coords.append([(x,x+w),(y,y+h),area])
-                            cv2.drawContours(img, cnt, -1, (0, 0, 0), 3)
-                
-                            cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-
-
-                        cv2.drawContours(img, cnt, -1, (0, 0, 0), 3)
-            
-                        cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
-                        break 
-                    else:
-                        print("OK {} is greater than {}".format(x,item_coords[coord][0][0]))
-                        print(x,item_coords[coord][0][0])
-                        itm_cnt += 1
-                        item_coords.append([(x,x+w),(y,y+h),area])
-                        cv2.drawContours(img, cnt, -1, (0, 0, 0), 3)
-            
-                        cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-                        
-            else:
-                print("COORDS LIST IS EMPTY, ADDING FIRST VALUE...")
-                itm_cnt += 1
-                item_coords.append([(x,x+w),(y,y+h),area])
-                cv2.drawContours(img, cnt, -1, (0, 0, 0), 3)
-            
-                cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-
-    while itm_cnt > 3: #TEMPORARY. THERE SHOULD NEVER BE MORE THAN THREE ORDERS.
-        itm_cnt -= 1
-        item_coords.pop(-1)
     cv2.imshow('img',img)
     cv2.imshow('greyinv',gray_inv)
     cv2.imshow('fillog',im_floodfill)
@@ -108,19 +72,17 @@ def getOrderItems(img):
     cv2.imshow("fill",final)
     cv2.imshow('blur',blur)
     cv2.imshow('canny',imgCanny)
-    #cv2.imshow('bw',blackAndWhiteImage)
+
     cv2.waitKey(5)
 
-    return itm_cnt, item_coords
+    return itm_cnt, item_coords, item_corners
 
-def getDomColor(img, coords):
-    colors = []
-    tmp_img = img[coords[0][1]:coords[1][1], coords[0][0]:coords[1][0]]
-    
-    for i in range(coords[0][1]-coords[0][0]): # loop each row of pixels. 
-        for j in range(coords[1][1]-coords[1][0]): # loop each pixel in row
-            (b,g,r) = tmp_img[j,i]
-            
-            colors.append((b,g,r))
 
-    return colors
+def classifyShape(sides,classify):
+    if classify == "side":
+        if sides <= 6:
+            return "cola"
+        else:
+            return "fries"
+    elif classify == "burger":
+        return "burger"
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Initializing...")
 from userInput import init, getTopOrderCoords, getBotOrderCoords
-from classifier import getOrderImg, getOrderItems, removeText, getDomColor
+from classifier import getOrderImg, getOrderItems, removeText, classifyShape
 import os
 
 
@@ -13,17 +13,31 @@ if __name__ == "__main__":
         orders = []
         img = getOrderImg(order_region) # Get the order
         img = removeText(img) # Remove any text
-        cnt, item_coords = getOrderItems(img) # Get order items
+        cnt, item_coords, corners = getOrderItems(img) # Get order items
         print("There is {} item(s)".format(cnt))
         print(item_coords)
-        """if cnt == 3: # orders of three always have the following items.
+
+        if cnt == 3: # orders of three always have the following items.
             orders.append("cola")
             orders.append("fries")
             # remove the items from the coords list since we don't need to scan them.
             item_coords.pop(2)
             item_coords.pop(1)
-            
-            for coord in item_coords:
-                color = getDomColor(img, coord) 
-                print(color)"""
+            corners.pop(2)
+            corners.pop(1)
+            print("Cola and fries automatically added.")
+            print("Burger has {} corners".format(corners[0]))
+            print(classifyShape(corners[0],"burger"))
+        
+        elif cnt == 2:
+            print("Burger has {} corners".format(corners[0]))
+            print(classifyShape(corners[0],"burger"))
+            print("Side has {} corners".format(corners[1]))
+            print(classifyShape(corners[1],"side"))
+
+        elif cnt == 1:
+            print("Burger has {} corners".format(corners[0]))
+            print(classifyShape(corners[0],"burger"))
+        else:
+            print("No customer")
         input("YES: ")
